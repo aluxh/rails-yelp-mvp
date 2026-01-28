49ec457a1eb3e7458f8aa582b0925487f4671f4f
Alex Ho <alex.hochunsiong@gmail.com>
2026-01-28 10:42:35 +0800

    - Add a average_rating method to Restaurant model - Use bootstrap and stars emojis to display average ratings in restaurant views - Update restaurant index and show views to include average ratings

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,4 +4,8 @@ class Restaurant < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
   validates :category, inclusion: { in: %w[chinese italian japanese french belgian] }
+
+  def average_rating
+    reviews.average(:rating).to_f.round(1)
+  end
 end
